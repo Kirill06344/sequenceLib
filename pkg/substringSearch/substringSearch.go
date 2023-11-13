c70bc84f162e689bd686e8d85a3ab8e1058dd712
stewie <kirill12093@mail.ru>
2023-11-13 18:19:24 +0300

    improve search

--- pkg/substringSearch/substringSearch.go
+++ pkg/substringSearch/substringSearch.go
@@ -1,11 +1,42 @@
 package substringSearch
 
 import (
+	"bufio"
 	"bytes"
+	"io"
 	"os"
+	"strings"
 )
 
-func Contains(filename string, substring string) (bool, error) {
+func Contains(filename string, substring string, readFileByLine bool) (bool, error) {
+	if readFileByLine {
+		return containsByLine(filename, substring)
+	}
+	return containsAllFile(filename, substring)
+}
+
+func containsByLine(filename string, substring string) (bool, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return false, err
+	}
+
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				return false, nil
+			}
+			return false, nil
+		}
+		if strings.Contains(line, substring) {
+			return true, nil
+		}
+	}
+}
+
+func containsAllFile(filename string, substring string) (bool, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		return false, err
